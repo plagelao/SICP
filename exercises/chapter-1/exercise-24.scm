8cc09e3c18652c20a00a0a9f85ceb14cf20e0f7b
Alberto <plagelao@gmail.com>
2011-07-16 13:16:04 +0100

    Adds exercise 1.24

--- exercises/chapter-1/exercise-24.scm
+++ exercises/chapter-1/exercise-24.scm
@@ -2,10 +2,6 @@
 
 (in-test-group exercise-24
                (define-each-test
-                 (assert-= (fast-smallest-divisor 2) 2)
-                 (assert-= (fast-smallest-divisor 199) 199)
-                 (assert-= (fast-smallest-divisor 1999) 1999)
-                 (assert-= (fast-smallest-divisor 19999) 7)
-                 (assert-in-delta (fast-search-for-primes 10000000000 10000000061 0) 0.47 0.1)
-                 (assert-in-delta (fast-search-for-primes 100000000000 100000000057 0) 1.48 0.1)
-                 (assert-in-delta (fast-search-for-primes 1000000000000 1000000000063 0) 4.67 0.1)))
+                 (assert-in-delta (fast-search-for-primes 10000000000 10000000061 0) 0.37 0.01)
+                 (assert-in-delta (fast-search-for-primes 100000000000 100000000057 0) 0.42 0.01)
+                 (assert-in-delta (fast-search-for-primes 1000000000000 1000000000063 0) 0.45 0.01)))
